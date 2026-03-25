190ae5cd1e2630fe6b8aee84656ce7433b8e43b1
mack6870 <mackenziefurlong@hotmail.com>
2026-03-25 19:00:58 -0400

    update to contact form

--- js/main.js
+++ js/main.js
@@ -340,50 +340,40 @@
 
         /* local validation */
 	    $('#contactForm').validate({
-
-            /* submit via ajax */
-            submitHandler: function(form) {
-
-                var sLoader = $('.submit-loader');
-
-                $.ajax({
-
-                    type: "POST",
-                    url: "inc/sendEmail.php",
-                    data: $(form).serialize(),
-                    beforeSend: function() {
-
-                        sLoader.slideDown("slow");
-
-                    },
-                    success: function(msg) {
-
-                        // Message was sent
-                        if (msg == 'OK') {
-                            sLoader.slideUp("slow");
-                            $('.message-warning').fadeOut();
-                            $('#contactForm').fadeOut();
-                            $('.message-success').fadeIn();
-                        }
-                        // There was an error
-                        else {
-                            sLoader.slideUp("slow");
-                            $('.message-warning').html(msg);
-                            $('.message-warning').slideDown("slow");
-                        }
-
-                    },
-                    error: function() {
-
-                        sLoader.slideUp("slow");
-                        $('.message-warning').html("Something went wrong. Please try again.");
-                        $('.message-warning').slideDown("slow");
-
-                    }
-
-                });
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                },
+                email: {
+                    required: true,
+                    email: true
+                },
+                subject: {
+                    required: true
+                },
+                message: {
+                    required: true,
+                    minlength: 15
+                }
+            },
+            messages: {
+                name: {
+                    required: "Please enter your name.",
+                    minlength: "Please enter your name."
+                },
+                email: {
+                    required: "Please enter a valid email address.",
+                    email: "Please enter a valid email address."
+                },
+                subject: {
+                    required: "Please enter a subject."
+                },
+                message: {
+                    required: "Please enter your message.",
+                    minlength: "Please enter your message (at least 15 characters)."
+                }
             }
-
         });
     };
 
